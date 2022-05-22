8432b803de09a2a3225022a74c828c6960ff2b14
Nexe <rohtirautela1408@gmail.com>
2022-05-22 15:50:42 +0530

    added orb detection.

---
+++ Assets/Scripts/Orb.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orb : MonoBehaviour, OrbInterface
+{
+    public void isDetected()
+    {
+
+    }
+}
+
+public interface OrbInterface
+{
+    public void isDetected();
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float runSpeed = 3.0f;
 
