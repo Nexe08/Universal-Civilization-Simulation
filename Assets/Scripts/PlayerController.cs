167354a7a55fb5fae73d99031c95cc6e90d48a72
Nexe <rohtirautela1408@gmail.com>
2022-05-22 14:41:46 +0530

    added top-down player movement.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float runSpeed = 3.0f;
+
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+    float moveLimiter = .7f;
+
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();    
+    }
+
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+
+    void FixedUpdate()
+    {
+        if (horizontal != 0 && vertical != 0)
+        {
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+        
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+}
