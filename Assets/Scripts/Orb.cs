812f5768a267d99f5b9dee289fe744f1e2aa01e8
Nexe <rohtirautela1408@gmail.com>
2022-05-29 15:04:34 +0530

    implemented: - add heart beat sfx. - add player animation

--- Assets/Scripts/Orb.cs
+++ Assets/Scripts/Orb.cs
@@ -33,18 +33,22 @@ public class Orb : MonoBehaviour
         switch (pro_man.GetCivilizationLevel()) {
             case 0:
                     HoldedPoint = Random.Range(1, 10);
+                    gameObject.transform.localScale = new Vector3(1,1,1);
                     break;
                 
                 case 1:
                     HoldedPoint = Random.Range(100, 1000);
+                    gameObject.transform.localScale = new Vector3(1.5f,1.5f,1.5f);
                     break;
                 
                 case 2:
                     HoldedPoint = Random.Range(10000, 100000);
+                    gameObject.transform.localScale = new Vector3(2,2,2);
                     break;
                 
                 case 3:
                     HoldedPoint = Random.Range(1000000, 100000000);
+                    gameObject.transform.localScale = new Vector3(2.5f,2.5f,2.5f);
                     break;
                 
             default :
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,30 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public AudioSource HeartBeat;
+    public bool PlayHeartBeatSFX = false; // will get enabled in animation player
     public float runSpeed = 3.0f;
 
     Rigidbody2D body;
+    Animation anim;
 
     float horizontal;
     float vertical;
     float moveLimiter = .7f;
 
 
-    void Start()
-    {
+    void Start(){
         body = GetComponent<Rigidbody2D>();    
+        anim = GetComponent<Animation>();
     }
 
 
-    void Update()
-    {
+    void Update(){
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        HandleSFX();
     }
 
 
-    void FixedUpdate()
-    {
+    void FixedUpdate(){
         if (horizontal != 0 && vertical != 0)
         {
             horizontal *= moveLimiter;
@@ -35,5 +38,18 @@ public class PlayerController : MonoBehaviour
         }
         
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+
+    protected void HandleSFX() {
+        if (HeartBeat.time == HeartBeat.clip.length) {
+            PlayHeartBeatSFX = false;
+        }
+
+        if (PlayHeartBeatSFX) {
+            if (HeartBeat.isPlaying) {
+                return;
+            }
+            HeartBeat.Play();
+        }
     }
 }
